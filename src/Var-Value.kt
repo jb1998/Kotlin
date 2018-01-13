7f01ade08a2ed00f5d8613d48966e2afc0af5d50
Jatin Bindra <jatinbindra171998@gmail.com>
2018-01-13 11:24:00 +0530

    learner

---
+++ src/Var-Value.kt
@@ -0,0 +1,14 @@
+fun main(args : Array<String>){
+    var a= 10
+    a=45
+    println(a)
+    val b=35
+   println(b+a)
+    var str="jatin bindra"
+    str="kotlin"
+    val str2 ="ada"
+    str="lala"
+    val temp : Int
+    temp=45
+    println("tell the compiler $temp")
+}
