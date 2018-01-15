f642ba8ed96997a03dd75181da63e5eb12eda6f1
Jatin Bindra <jatinbindra171998@gmail.com>
2018-01-15 18:33:53 +0530

    learner

--- src/If-When.kt
+++ src/If-When.kt
@@ -4,13 +4,13 @@ fun main(args: Array<String>) {
     var sc=Scanner(System.`in`)
     var a=sc.nextInt()
     var b=sc.nextInt()
-//    var ans=if(a>b)
-//             {
-//                  "a is greater"
-//             }else{
-//         "b is greater"
-//    }
-//    print(ans)
+    var ans=if(a>b)
+             {
+                  "a is greater"
+             }else{
+         "b is greater"
+    }
+    print(ans)
 
    var ans2= when(a){
         1 ->"a is one"
