7c6e815806733450592f0129eb847bea89cc4533
Jatin Bindra <jatinbindra171998@gmail.com>
2018-01-16 05:56:25 +0530

    learner

---
+++ src/Inheritance.kt
@@ -0,0 +1,22 @@
+fun main(args : Array<String>){
+var obj=Bmw()
+    obj.colorbmw()
+    obj.color()
+    println(obj.speed)
+
+}
+open class Car{
+    var speed:Int=0
+    var name:String=""
+
+    fun color(){
+        println("this is car class")
+    }
+}
+class Bmw:Car(){
+    var cost:Int=0;
+    fun colorbmw(){
+        println("this is bmw class")
+      speed=23
+    }
+}
