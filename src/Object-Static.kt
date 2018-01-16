f114d61de4c79f44a6f77b57e12e2c324f083eea
Jatin Bindra <jatinbindra171998@gmail.com>
2018-01-17 05:04:00 +0530

    learner

--- src/Object-Static.kt
+++ src/Object-Static.kt
@@ -1,11 +1,20 @@
 fun main(args: Array<String>) {
     println(Abcd.a)
     println(Abcd.helper())
+    print(Abcd.P.a)
 }
 object Abcd{
     var a=10  //static
     fun helper():String{     //static
     return "jatin"
     }
+class P{
+    companion object {
+        var a=10  //static
+        fun helper():String{     //static
+            return "jatin"
+        }
+    }
 
+}
 }
